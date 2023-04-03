5712a9e635054e4024bcf2bdeb6fa8b30fe1b58f
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-03 22:00:12 +0530

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,25 +1,39 @@
-
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-const isAuthenticated = () => {
-  return (
-    localStorage.getItem("username") &&
-    localStorage.getItem("password")
-  );
-};
+function PrivateRoute({ children, ...rest }) {
+  const isLoggedIn = localStorage.getItem("username") && localStorage.getItem("password");
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      isAuthenticated() ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/login" />
-      )
-    }
-  />
-);
+  return (
+    <Route {...rest} render={() => {
+      return isLoggedIn ? children : <Redirect to="/login" />;
+    }} />
+  );
+}
 
 export default PrivateRoute;
+
+// import React from "react";
+// import { Route, Redirect } from "react-router-dom";
+
+// const isAuthenticated = () => {
+//   return (
+//     localStorage.getItem("username") &&
+//     localStorage.getItem("password")
+//   );
+// };
+
+// const PrivateRoute = ({ component: Component, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={(props) =>
+//       isAuthenticated() ? (
+//         <Component {...props} />
+//       ) : (
+//         <Redirect to="/login" />
+//       )
+//     }
+//   />
+// );
+
+// export default PrivateRoute;
